7386c9c9f1660cea975a17bbdd9fcccd9f193ea9
syk-ohayashi <s-hayashi@inglow.jp>
2020-07-29 16:36:13 +0900

    キャンペーン一覧画面の実装

--- campaign/views.py
+++ campaign/views.py
@@ -1,19 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
+from .models import Campaign
+from .forms import CampaignForm
 
 # Create your views here.
 
 
 def campaign_list(request):
-  return render(request, 'campaign/index.html', {})
+	campaigns = Campaign.objects.all()
+	page_obj = paginate_queryset(request, campaigns, 5)
+	return render(request, 'campaign/index.html', {
+		'campaigns':page_obj.object_list,
+		'paginator':page_obj
+	})
+
+def paginate_queryset(request, queryset, count):
+	paginator = Paginator(queryset, count)
+	page = request.GET.get('page')
+	try:
+		page_obj = paginator.page(page)
+	except PageNotAnInteger:
+		page_obj = paginator.page(1)
+	except EmptyPage:
+		page_obj = paginator.page(paginator.num_pages)
+	return page_obj
+
+
 
 
 def campaign_form(request):
-  return render(request, 'campaign/form.html', {})
+	post_data = request.POST
+	#post_data['campany_id'] = [request.user.campany_id]
+	form = CampaignForm(post_data or None, initial={'campany_id':request.user.campany_id})
+	#print(form)
+	
+	if form.is_valid():
+		Campaign.objects.create(**form.cleaned_data)
+		return redirect('campaign_list')
+	
+	return render(request, 'campaign/form.html', {'form':form})
 
 
 def campaign_group(request):
-  return render(request, 'campaign/group_list.html', {})
+	return render(request, 'campaign/group_list.html', {})
 
 
 def material_upload(request):
-  return render(request, 'campaign/material_upload.html')
+	return render(request, 'campaign/material_upload.html')

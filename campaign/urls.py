dad6733b97b5b4ee7384264ba858c15aa052172f
terazawa <t-terazawa@inglow.jp>
2020-07-30 10:16:49 +0900

    no message

--- campaign/urls.py
+++ campaign/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('campaign_form', views.campaign_form, name="campaign_form"),
     path('campaign_update/<int:pk>', views.campaign_update, name="campaign_update"),
     path('campaign_group', views.campaign_group, name="campaign_group"),
-    path('material_upload', views.material_upload, name="material_upload"),
 ]

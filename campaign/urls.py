e0d7639bd9e3859bc79a13a173543dcbc0947312
おはやし <s-hayashi@inglow.jp>
2020-07-30 12:17:40 +0900

    キャンペーンの削除機能を実装しました。

--- campaign/urls.py
+++ campaign/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('campaign_list', views.campaign_list, name="campaign_list"),
     path('campaign_form', views.campaign_form, name="campaign_form"),
     path('campaign_update/<int:pk>', views.campaign_update, name="campaign_update"),
+    path('campaign_delete/<int:pk>', views.campaign_delete, name="campaign_delete"),
     path('campaign_group', views.campaign_group, name="campaign_group"),
 ]
